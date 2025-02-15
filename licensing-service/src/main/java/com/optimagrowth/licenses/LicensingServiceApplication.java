b041ad196d67e62d2871a1be84a1fc652d9c1c3f
Gaurav Pandey <gauravkumar.pandey@dxc.com>
2025-02-15 14:46:22 +0000

    Intial prohect structure commit

---
+++ licensing-service/src/main/java/com/optimagrowth/licenses/LicensingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.optimagrowth.licenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(LicensingServiceApplication.class, args);
+  }
+}
